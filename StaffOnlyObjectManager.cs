53347ef38fda02bff0ea3e664b797533ba443e3b
pogapoganyaa <100682474+pogapoganyaa@users.noreply.github.com>
2022-12-08 20:44:52 +0900

    スタッフ限定のオブジェクトの表示・非表示管理

---
+++ StaffOnlyObjectManager.cs
@@ -0,0 +1,46 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace PogapogaGimmick
+{
+    public class StaffOnlyObjectManager : UdonSharpBehaviour
+    {
+        public string[] staffNames;
+        public GameObject[] staffOnlyEnableObject;
+        public GameObject[] staffOnlyDisableObject;
+
+        void Start()
+        {
+            VRCPlayerApi playerApi = Networking.LocalPlayer;
+
+            foreach (string name in staffNames)
+            {
+                // スタッフの処理
+                if (playerApi.displayName == name)
+                {
+                    foreach (GameObject gameObject in staffOnlyEnableObject)
+                    {
+                        gameObject.SetActive(true);
+                    }
+                    foreach (GameObject gameObject in staffOnlyDisableObject)
+                    {
+                        gameObject.SetActive(false);
+                    }
+                    return;
+                }
+            }
+
+            // スタッフ以外の処理
+            foreach (GameObject gameObject in staffOnlyEnableObject)
+            {
+                gameObject.SetActive(false);
+            }
+            foreach (GameObject gameObject in staffOnlyDisableObject)
+            {
+                gameObject.SetActive(true);
+            }
+        }
+    }
+}
+
